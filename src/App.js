11c4859b0ad038bfb42471f8b491a34d0f32b59d
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-10-26 12:06:07 -0400

    use blank background or image background

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import './App.css';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Footer from "./components/Footer"
 import Services from './pages/Services';
 import Team from './pages/Team';
 import Stake from './pages/Stake';
@@ -19,6 +20,7 @@ function App() {
           <Route path='/team' component={Team} />
           <Route path='/stake' component={Stake} />
         </Switch>
+        <Footer/>
       </Router>
     </>
   );
